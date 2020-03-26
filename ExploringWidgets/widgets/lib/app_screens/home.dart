024c0ad1a99632363ff4d7cdbb6586f66a36a322
TejusAcentria <Tejus.gupta@acentriatech.com>
2020-03-26 12:06:14 +0530

    'add_demo_of_row_and_columns'

---
+++ ExploringWidgets/widgets/lib/app_screens/home.dart
@@ -0,0 +1,137 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Container(
+        padding: EdgeInsets.only(left: 10.0,top: 40.0),
+          alignment: Alignment.center,
+          color: Colors.deepPurple,
+          child: Column(
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                    "Spice Jet",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 15.0,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white),
+                  )),
+                  Expanded(
+                      child: Text(
+                    "From Jaipur to Delhi",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 15.0,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white),
+                  )),
+                  Expanded(
+                      child: Text(
+                    "Deprature",
+                    textDirection: TextDirection.ltr,
+                    style: TextStyle(
+                        decoration: TextDecoration.none,
+                        fontSize: 15.0,
+                        fontFamily: 'Raleway',
+                        fontWeight: FontWeight.w300,
+                        fontStyle: FontStyle.italic,
+                        color: Colors.white),
+                  )),
+                ],
+              ),
+              Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                        "Spice Jet",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 15.0,
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white),
+                      )),
+                  Expanded(
+                      child: Text(
+                        "From Jaipur to Delhi",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 15.0,
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white),
+                      )),
+                  Expanded(
+                      child: Text(
+                        "Deprature",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 15.0,
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white),
+                      )),
+                ],
+              ), Row(
+                children: <Widget>[
+                  Expanded(
+                      child: Text(
+                        "Spice Jet",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 15.0,
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white),
+                      )),
+                  Expanded(
+                      child: Text(
+                        "From Jaipur to Delhi",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 15.0,
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white),
+                      )),
+                  Expanded(
+                      child: Text(
+                        "Deprature",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            decoration: TextDecoration.none,
+                            fontSize: 15.0,
+                            fontFamily: 'Raleway',
+                            fontWeight: FontWeight.w300,
+                            fontStyle: FontStyle.italic,
+                            color: Colors.white),
+                      )),
+                ],
+              )
+            ],
+          )),
+    );
+  }
+}
