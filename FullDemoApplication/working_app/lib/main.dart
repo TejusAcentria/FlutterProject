77e68cf4ba0d034a5d7cc211885e45b5f26389c9
TejusAcentria <Tejus.gupta@acentriatech.com>
2020-03-26 16:02:47 +0530

    'added_dropDown'

--- FullDemoApplication/working_app/lib/main.dart
+++ FullDemoApplication/working_app/lib/main.dart
@@ -16,6 +16,8 @@ class FavouriteCity extends StatefulWidget {
 
 class _FavouriteCityState extends State<FavouriteCity> {
   String nameCity = "";
+  var currrency = ['Rupees', 'Dollar', 'Pounds'];
+  var _currenctValueSelect = 'Rupees';
 
   @override
   Widget build(BuildContext context) {
@@ -33,23 +35,43 @@ class _FavouriteCityState extends State<FavouriteCity> {
 //                    nameCity = userInput;
 //                });
 //              },
-            maxLength: 10,
-            onChanged: (String userInput) {
+              maxLength: 10,
+              onChanged: (String userInput) {
                 setState(() {
-                    nameCity = userInput;
+                  nameCity = userInput;
                 });
               },
+            ),
+            DropdownButton<String>(
+              items: currrency.map((String dropDownStrinItem) {
+                return DropdownMenuItem<String>(
+                  value: dropDownStrinItem,
+                  child: Text(dropDownStrinItem),
+                );
+              }).toList(),
+              onChanged: (String newValueSelect) {
+                _onDropDownItemSelected(newValueSelect);
+
+              },
+              value: currrency[2],
             ),
             Padding(
                 padding: EdgeInsets.all(30.0),
                 child: Text(
-                  "Your city is $nameCity",
+                  "Your city is $_currenctValueSelect",
                   style: TextStyle(fontSize: 15.0),
                 ))
           ],
         ),
       ),
     );
+  }
+
+  void _onDropDownItemSelected(String newValueSelect) {
+
+    setState(() {
+      this._currenctValueSelect=newValueSelect;
+    });
   }
 }
 
